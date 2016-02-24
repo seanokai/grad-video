eac730aeb16538cf8998bd7ce21ef3eca15f0376
Sean Okai <seanokai@hotmail.com>
2016-02-23 23:33:01 -0500

    feat(js): added play and pause button
    added play and pause button to the centre of the video player

--- js/main.js
+++ js/main.js
@@ -4,8 +4,14 @@ var video = $(".gd-grad-vid").get(0);
 $btn.on('click', function () {
 	if (video.paused) {
 		video.play();
-
+		$btn.addClass('pause');
+		console.log('play');
+		$btn.removeClass('play');
 	} else {
 		video.pause();
+		$btn.addClass('play')
+		console.log('pause');
+		$btn.removeClass('pause');
 	}
 });
+
